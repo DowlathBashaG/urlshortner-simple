c54f2d6bd17259a0c136bb9f51a01bcad6eed26c
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2024-04-17 10:21:10 +0530

    initial commit  create and get short url

---
+++ urlshortnersimple/src/main/java/dowlath/io/urlshortnersimple/UrlshortnersimpleApplication.java
@@ -0,0 +1,13 @@
+package dowlath.io.urlshortnersimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlshortnersimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlshortnersimpleApplication.class, args);
+	}
+
+}
---
+++ urlshortnersimple/src/test/java/dowlath/io/urlshortnersimple/UrlshortnersimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package dowlath.io.urlshortnersimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortnersimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
